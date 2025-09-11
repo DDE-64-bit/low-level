8af19155ee4bde2ec5443f74d3871193052ac436
DDE-64-bit <olivier.hartsuiker@gmail.com>
2025-09-11 17:53:07 +0800

    created 3.c

--- c/3.c
+++ c/3.c
@@ -3,12 +3,12 @@
 # include <stdlib.h>
 
 struct Contact{
-    char name[128];
-    char phone[32];
+    char name[64];
+    char phone[16];
     unsigned int age;
 };
 
-void RemoveNewLine(char *buf){
+void removeNewLine(char *buf){
     size_t len = strlen(buf);
     if (len > 0 && buf[len-1] == '\n'){
         buf[len-1] = '\0';
@@ -16,32 +16,29 @@ void RemoveNewLine(char *buf){
 }
 
 int main(){
-    char name[128];
-    char phone[32];
-    char agebuf[64];
+    char name[64];
+    char phone[16];
+    char ageBuf[128];
 
-    printf("What is your name?\n");
+    printf("What is your name? \n");
     fgets(name, sizeof(name), stdin);
-    RemoveNewLine(name);
+    removeNewLine(name);
 
-    printf("\nWhat is your phone number?\n");
+    printf("What is your phone number? \n");
     fgets(phone, sizeof(phone), stdin);
-    RemoveNewLine(phone);
+    removeNewLine(phone);
 
-    printf("\nWhat is your age?\n");
-    fgets(agebuf, sizeof(agebuf), stdin);
-    RemoveNewLine(agebuf);
+    printf("What is your age?");
+    fgets(ageBuf, sizeof(ageBuf), stdin);
+    removeNewLine(ageBuf);
 
-    unsigned int age = (unsigned int) strtoul(agebuf, NULL, sizeof(agebuf));
+    unsigned int age = (unsigned int) strtoul(ageBuf, NULL, sizeof(ageBuf));
 
-    struct Contact Persoon;
+    struct Contact Person;
 
-    Persoon.age = age;
+    strcpy(Person.name, name);
+    strcpy(Person.phone, phone);
+    Person.age = age;
 
-    strcpy(Persoon.name, name);
-    strcpy(Persoon.phone, phone);
 
-    printf("%s", Persoon);
-
-    return 0;
 }
