ca05be28e9957e1cd4faa172d17f501519c9af80
DDE-64-bit <olivier.hartsuiker@gmail.com>
2025-09-12 03:12:27 +0800

    added extra features

--- c/3.c
+++ c/3.c
@@ -16,29 +16,61 @@ void removeNewLine(char *buf){
 }
 
 int main(){
-    char name[64];
-    char phone[16];
-    char ageBuf[128];
+    struct Contact contacts[10];
+    
+    int i = 1;
+    int contactCount = 0;
 
-    printf("What is your name? \n");
-    fgets(name, sizeof(name), stdin);
-    removeNewLine(name);
+    while(i){
+        char buf[4];
 
-    printf("What is your phone number? \n");
-    fgets(phone, sizeof(phone), stdin);
-    removeNewLine(phone);
+        printf("Do you want to create or search contacts?\n");
+        printf("[+] 1. Create new contact\n");
+        printf("[+] 2. Search for contact\n");
+        printf("[+] 3. Exit\n");
 
-    printf("What is your age?");
-    fgets(ageBuf, sizeof(ageBuf), stdin);
-    removeNewLine(ageBuf);
+        fgets(buf, sizeof(buf), stdin);
+        removeNewLine(buf);
 
-    unsigned int age = (unsigned int) strtoul(ageBuf, NULL, sizeof(ageBuf));
+        int input = (int) strtol(buf, NULL, 10);
+        if (input == 1){
+            char name[64];
+            char phone[16];
+            char ageBuf[128];
 
-    struct Contact Person;
+            printf("\nWhat is your name? \n");
+            fgets(name, sizeof(name), stdin);
+            removeNewLine(name);
 
-    strcpy(Person.name, name);
-    strcpy(Person.phone, phone);
-    Person.age = age;
+            printf("What is your phone number? \n");
+            fgets(phone, sizeof(phone), stdin);
+            removeNewLine(phone);
 
+            printf("What is your age? \n");
+            fgets(ageBuf, sizeof(ageBuf), stdin);
+            removeNewLine(ageBuf);
 
+            unsigned int age = (unsigned int) strtoul(ageBuf, NULL, 10);
+
+            strcpy(contacts[contactCount].name, name);
+            strcpy(contacts[contactCount].phone, phone);
+            contacts[contactCount].age = age;
+
+            for (int j = 0; j < 10; j++){
+                printf("Name: %s, Phone: %s, Age: %d \n", contacts[j].name, contacts[j].phone, contacts[j].age);
+            }
+
+            contactCount++;
+        }
+        else if (input == 2){
+            // search contacts
+        }
+        else if (input == 3){
+            return 1;
+        }
+        else{
+            printf("Not a valid input chosen...");
+        }
+    }
+    return 0;
 }
