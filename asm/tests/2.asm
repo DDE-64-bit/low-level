cd4a6752f4e15306e429cfec778351f83dcee6e9
DDE-64-bit <olivier.hartsuiker@gmail.com>
2025-09-22 16:16:11 +0200

    added asm files

--- asm/tests/2.asm
+++ asm/tests/2.asm
@@ -9,6 +9,7 @@ _start:
     push 9
     push 6
     push 3
+    push 1
 
     pop rax
     call _printRAXDigit
@@ -19,11 +20,18 @@ _start:
     pop rax
     call _printRAXDigit
 
-    call _exit
-
-_exit:
+    pop rax
+    cmp rax, 4
+    jne _errorExit ; when rax != 4 goto _notEqual
+    
     mov rax, 60
-    mov rdi, 0
+    mov rax, 0
+    syscall
+
+
+_errorExit:
+    mov rax, 60
+    mov rdi, 1
     syscall
 
 _printRAXDigit:
