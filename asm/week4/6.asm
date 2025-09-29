a10b93a4d7e43ebdbc91da00cb3ec36ca77491f3
DDE-64-bit <olivier.hartsuiker@gmail.com>
2025-09-29 11:49:51 +0200

    added more comments

--- asm/week4/6.asm
+++ asm/week4/6.asm
@@ -1,11 +1,15 @@
 section .data
-    inputText: db "Input: "
+    inputText: db "Single number: "
     inputLen equ $ - inputText
-    printText: db "Print Text", 10
-    printTextLen equ $ - printText
+    getPrintText: db "Please enter a text to print: "
+    getPrintTextLen equ $ - getPrintText
+
+    ; make a buf for r12b (single byte buffer)
+    buf: db 0
 
 section .bss
-    input: resb 10 ; 9 + \n
+    input: resb 2 ; number + \n
+    printText: resb 32
 
 section .text
     global _start
@@ -22,11 +26,64 @@ _start:
     mov rax, 0
     mov rdi, 0
     lea rsi, [rel input]
-    mov rdx, 9
+    mov rdx, 2
     syscall
 
-    
+    ; print get print text
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, [rel getPrintText]
+    mov rdx, getPrintTextLen
+    syscall
 
+    ; get print text
+    mov rax, 0
+    mov rdi, 0
+    lea rsi, [rel printText]
+    mov rdx, 32
+    syscall
+
+    ; save amount of input bytes
+    mov rbx, rax
+
+
+    ; print text 1 time
+
+    ; mov rax, 1
+    ; mov rdi, 1
+    ; lea rsi, [rel printText];
+    ; mov rdx, rbx
+    ; syscall
+
+    ; get info for loop
+
+    ; reg, use
+    ; rbx,          amount of input bytes
+    ; rbp (bl),     loop counter
+    ; r12 (r12b),   number
+
+    ; set bl (8bit) to 0
+    xor bl, bl
+
+    ; move byte to al
+    mov r12b, byte [input]
+
+    ; ptr to r12b
+    mov byte [buf], r12b
+
+    ; debug print to check result
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, byte [buf]
+    mov rdx, 1
+    syscall
+
+    ; use lea rsi, [input] to get access to the needed value
+
+
+
+    ; start loop
+    _loop:
 
     ; exit
     mov rax, 60
